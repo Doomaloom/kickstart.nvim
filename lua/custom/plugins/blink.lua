e2d0104a5b46d3ae2c4b2b25e96a90ae4405d493
Frank Kocun <frankkocun@Franks-MacBook-Air.local>
2025-09-12 02:13:25 -0400

    my first nvim setup

---
+++ lua/custom/plugins/blink.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim' },
+    config = function()
+      local blink = require 'blink.cmp'
+
+      blink.setup {
+        -- Enable LSP
+        sources = {
+          default = { 'lsp', 'path', 'buffer' },
+        },
+
+        -- Keymaps for completion
+        keymap = {
+          preset = 'default',
+          ['<CR>'] = { 'accept', 'fallback' },
+          ['<Tab>'] = { 'select_next', 'fallback' },
+          ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        },
+      }
+
+      -- Mason ensures servers are installed
+      require('mason').setup()
+      require('mason-lspconfig').setup {
+        ensure_installed = { 'pyright', 'clangd', 'lua_ls' },
+        handlers = {
+          function(server)
+            require('lspconfig')[server].setup {}
+          end,
+        },
+      }
+    end,
+  },
+}
---
+++ lua/custom/plugins/fugitive.lua
@@ -0,0 +1,6 @@
+return {
+  {
+    'tpope/vim-fugitive',
+    cmd = { 'Git', 'G' }, -- loads only when you use these commands
+  },
+}
